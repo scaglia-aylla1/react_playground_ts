fdbeaf60c619481455be6fd64b3c57f160cab5f4
Aylla Scaglia <aylla@scaglia.com.br>
2025-06-17 13:28:56 -0300

    :sparkles: Aprendendo React

---
+++ src/pages/contador/Contador.tsx
@@ -0,0 +1,23 @@
+﻿import { useState } from "react";
+
+function Contador() {
+
+    const [valor, setValor] = useState(0);
+
+    function handleClick() {
+        setValor(valor + 1);
+    }
+    function handleClick2() {
+        setValor(valor - 1);
+    }
+    return (
+        <div>
+            <h2>Componente Contador</h2>
+            <p>O valor atual é: {valor}</p>
+            <button onClick={handleClick}>Adicionar 1</button>
+            <button onClick={handleClick2}>Subtrair 1</button>
+        </div>
+    )
+}
+
+export default Contador
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,22 @@
+﻿
+
+interface homeProps{
+  titulo: string;
+  texto: string;
+}
+
+
+
+
+
+function Home(props: homeProps) {
+  return (
+    <div>
+      <h1>Componente Home</h1>
+      <h2>{props.titulo}</h2>
+      <p>{props.texto}</p>
+    </div>
+  )
+}
+
+export default Home
